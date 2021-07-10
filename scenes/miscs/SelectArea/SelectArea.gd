e30b471e7159ae6fc5925aa00fc8f636ff08155e
Sebajuste <sebajuste@hotmail.com>
2021-07-10 15:28:38 +0200

    Change picking method with raycast

--- scenes/miscs/SelectArea/SelectArea.gd
+++ scenes/miscs/SelectArea/SelectArea.gd
@@ -1,10 +1,7 @@
 extends Area
 
 
-signal object_selected(object)
-
-
-export var group_target := ""
+onready var object := owner
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,17 +13,3 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
-func _on_SelectArea_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
-	
-	if event is InputEventMouseButton:
-		
-		if event.button_index == BUTTON_LEFT and event.pressed:
-			print("event in selector")
-			var target := owner
-			
-			if group_target == "" or target.is_in_group(group_target):
-				emit_signal("object_selected", target)
-				if ObjectSelector:
-					ObjectSelector.emit_signal("object_selected", target, self)
-	
--- scenes/miscs/SelectorHandler/SelectorHandler.gd
+++ scenes/miscs/SelectorHandler/SelectorHandler.gd
@@ -15,7 +15,7 @@ var select_timer := 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	ObjectSelector.connect("object_selected", self, "_on_object_selected")
+	#ObjectSelector.connect("object_selected", self, "_on_object_selected")
 	
 	pass # Replace with function body.
 
@@ -28,6 +28,62 @@ func _process(delta):
 	pass
 
 
+func _input(event):
+	
+	if event is InputEventMouseButton:
+		
+		if event.button_index == BUTTON_LEFT:
+			
+			var mouse_pos := get_viewport().get_mouse_position()
+			
+			var camera := get_tree().get_root().get_camera()
+			
+			var from := camera.project_ray_origin(mouse_pos)
+			var to := from + camera.project_ray_normal(mouse_pos) * 1000
+			
+			var space_state := camera.get_world().direct_space_state
+			var result := space_state.intersect_ray(from, to, [], 0x0400, false, true)
+			
+			print("pick result : ", result)
+			
+			if result and result.has("collider"):
+				
+				var select_area : Area = result.collider
+				var object : Spatial = select_area.object
+				
+				if not target_ref or target_ref.get_ref() == null or target_ref.get_ref() != object:
+					
+					var select_hint
+					
+					if select_hint_ref != null and select_hint_ref.get_ref() != null:
+						select_hint = select_hint_ref.get_ref()
+					else:
+						select_hint = SELECT_HINT_SCENE.instance()
+					
+					select_area.add_child(select_hint)
+					
+					target_ref = weakref(object)
+					select_hint_ref = weakref(select_hint)
+					
+					select_timer = 0.0
+				
+			elif not result or not result.has("collider"):
+				
+				if select_timer > select_await:
+					
+					if select_hint_ref != null:
+						
+						var select_hint = select_hint_ref.get_ref()
+						if select_hint == null:
+							select_hint_ref = null
+						else:
+							select_hint.queue_free()
+						
+						target_ref = null
+				
+	
+
+"""
 func _unhandled_input(event):
 	
 	if event is InputEventMouseButton:
@@ -43,7 +99,7 @@ func _unhandled_input(event):
 					select_hint.queue_free()
 				
 			target_ref = null
-
+"""
 
 func get_target():
 	if target_ref:
