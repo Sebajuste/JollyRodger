82e9ecbe54b10cc5f61a6eafb1bb776599493550
Nono02P <nono02p@hotmail.fr>
2021-07-10 21:30:18 +0200

    Adding details (grass, flowers and palm tree).
    There is a bug on the palm tree, it appears at the wrong place.

--- scenes/games/Island/Island.gd
+++ scenes/games/Island/Island.gd
@@ -4,83 +4,129 @@ extends Node
 const HTerrain = preload("res://addons/zylann.hterrain/hterrain.gd")
 const HTerrainData = preload("res://addons/zylann.hterrain/hterrain_data.gd")
 const HTerrainTextureSet = preload("res://addons/zylann.hterrain/hterrain_texture_set.gd")
+const HTerrainDetailLayer = preload("res://addons/zylann.hterrain/hterrain_detail_layer.gd")
+#const Palm = preload("res://scenes/objects/Palm/Palm.tscn")
 
+export(Shader) var detail_shader
+export(Shader) var palm_shader
 export(OpenSimplexNoise) var noise
-export(OpenSimplexNoise) var flower_noise
+export(OpenSimplexNoise) var models_noise
 export(Curve) var height_curve : Curve
-export(float) var island_height = 200.0
-export(float) var sand_level = 25
+export(Curve) var island_curve : Curve
+export(float) var wind = 0.4
+export(float) var grass_density = 4
+export(float) var flower_density = 1
+export(float) var palm_density = 0.04
+export(float, 0, 1) var grass_on_sand = 0.6
+export(float, 0, 1) var grass_on_rock = 0.5
+export(float) var island_height = 150
+export(float) var sand_level = 15
 export(float) var terrain_size = 513
-var min_center_distance = terrain_size / 4
-var max_center_distance = terrain_size / 2
-var island_size : float = rand_range(min_center_distance, max_center_distance)
+
+#export(int) var max_palms = 500
+#export(float) var min_palms_gap = 20
+#export(float, 0, 1) var palms_perlin_threshold = 0.6
+export(float) var palms_height_threshold = 40
+
+var island_size : float = terrain_size / 2.3
+var beach_size : float = terrain_size / 2
 # You may want to change paths to your own textures
 
 var grass_texture = load("res://assets/2d/textures/terrain/TexturesCom_Grass0157_1_seamless_S.jpg")
 var sand_texture = load("res://assets/2d/textures/terrain/Sand_007_basecolor.jpg")
 var rock_texture = load("res://assets/2d/textures/terrain/TexturesCom_CliffRock_A_2x2_1K_albedo.jpg")
-var flower_texture = load("res://assets/2d/textures/terrain/Rocks006_1K-JPG/Rocks006_1K_Color.jpg")
+var grass_detail = load("res://assets/2d/textures/grass.png")
+var flower_detail = load("res://assets/2d/textures/grass2.png")
 
 func _ready():
 	# Create terrain resource and give it a size.
 	# It must be either 513, 1025, 2049 or 4097.
+	
 	var terrain_data = HTerrainData.new()
 	terrain_data.resize(terrain_size)
-
 	
-
+	var terrain_childs = []
+	
+	# Generate grass detail layer
+	terrain_childs.append(generate_detail_layer_texture(terrain_data, grass_detail, grass_density, detail_shader, 0))
+	
+	# Generate flowers detail layer
+	terrain_childs.append(generate_detail_layer_texture(terrain_data, flower_detail, flower_density, detail_shader, 1))
+	
+	# Generate palms detail layer
+	terrain_childs.append(generate_detail_layer_mesh(terrain_data, get_palm(), palm_density, palm_shader, 2))
+	
 	# Get access to terrain maps
-	var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
-	var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
-	var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
-
-	heightmap.lock()
-	normalmap.lock()
-	splatmap.lock()
-	var center = Vector2(heightmap.get_width() / 2, heightmap.get_height() / 2)
-
+	var heightMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
+	var normalMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
+	var splatMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
+	var grassMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_DETAIL, 0)
+	var flowerMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_DETAIL, 1)
+	var palmMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_DETAIL, 2)
+	
+	var center = Vector2(heightMap.get_width() / 2, heightMap.get_height() / 2)
+	var grass_prct_sand = 1 - grass_on_sand
+	var grass_prct_rock = 1 - grass_on_rock
+	
+	heightMap.lock()
+	normalMap.lock()
+	splatMap.lock()
+	grassMap.lock()
+	flowerMap.lock()
+	palmMap.lock()
+	
 	# Generate terrain maps
 	# Note: this is an example with some arbitrary formulas,
 	# you may want to come up with your owns
-	for z in heightmap.get_height():
-		for x in heightmap.get_width():
-			# Generate height			
-			var h = island_height * noise.get_noise_2d(x, z) * get_island_factor(x, z, center)
-
+	for z in heightMap.get_height():
+		for x in heightMap.get_width():
+			# Generate height
+			var h = generate_height(x, z, center)
+			
 			# Getting normal by generating extra heights directly from noise,
-			# so map borders won't have seams in case you stitch them
-			var h_right = island_height * noise.get_noise_2d(x + 0.1, z) * get_island_factor(x + 0.1, z, center)
-			var h_forward = island_height * noise.get_noise_2d(x, z + 0.1) * get_island_factor(x, z + 0.1, center)
+			var h_right = generate_height(x + 0.1, z, center)
+			var h_forward = generate_height(x, z + 0.1, center)
 			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
 			
 			# Generate texture amounts
-			var splat = splatmap.get_pixel(x, z)
+			var splat = splatMap.get_pixel(x, z)
 			var slope = 5.0 * normal.dot(Vector3.UP) - 2.0
 			
-			# Sand below sea level
-			var sand_amount = clamp(sand_level - h, 0.0, 1.0)
+			# Sand depending of the slope and a sand level
+			var sand_amount = clamp(1 - slope + sand_level - h, 0.0, 1.0)
 			# Rock on the slopes
 			var rock_amount = clamp(1.0 - slope, 0.0, 1.0)
-			# Randomly put flowers where there is no rock and no sand
-			var flower_amount = clamp(flower_noise.get_noise_2d(x, z) - rock_amount - sand_amount, 0, 1)
+			
+			# Add details where there is no rock and not too much sand
+			var grass_amount = clamp(1 - rock_amount * grass_prct_rock - sand_amount * grass_prct_sand, 0, 1)
+			var flower_amount = clamp(1 - rock_amount - sand_amount, 0, 1)
+			
+			var palm_amount = clamp(h - palms_height_threshold, 0, 1)
+			palm_amount = clamp(palm_amount - rock_amount, 0, 1)
 			
 			splat = splat.linear_interpolate(Color(0,1,0,0), sand_amount)
 			splat = splat.linear_interpolate(Color(0,0,1,0), rock_amount)
-			splat = splat.linear_interpolate(Color(0,0,0,1), flower_amount)
+			
+			heightMap.set_pixel(x, z, Color(h, 0, 0))
+			normalMap.set_pixel(x, z, HTerrainData.encode_normal(normal))
+			splatMap.set_pixel(x, z, splat)
+			grassMap.set_pixel(x, z, Color(grass_amount,0,0,0))
+			flowerMap.set_pixel(x, z, Color(flower_amount,0,0,0))
+			palmMap.set_pixel(x, z, Color(0,0,0,0))
 
-			heightmap.set_pixel(x, z, Color(h, 0, 0))
-			normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
-			splatmap.set_pixel(x, z, splat)
-
-	heightmap.unlock()
-	normalmap.unlock()
-	splatmap.unlock()
+	heightMap.unlock()
+	normalMap.unlock()
+	splatMap.unlock()
+	grassMap.unlock()
+	flowerMap.unlock()
+	palmMap.unlock()
 
 	# Commit modifications so they get uploaded to the graphics card
-	var modified_region = Rect2(Vector2(), heightmap.get_size())
+	var modified_region = Rect2(Vector2(), heightMap.get_size())
 	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
 	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
 	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_DETAIL)
 
 	# Create texture set
 	# NOTE: usually this is not made from script, it can be built with editor tools
@@ -92,20 +138,64 @@ func _ready():
 	texture_set.set_texture(1, HTerrainTextureSet.TYPE_ALBEDO_BUMP, sand_texture)
 	texture_set.insert_slot(-1)
 	texture_set.set_texture(2, HTerrainTextureSet.TYPE_ALBEDO_BUMP, rock_texture)
-	texture_set.insert_slot(-1)
-	texture_set.set_texture(3, HTerrainTextureSet.TYPE_ALBEDO_BUMP, flower_texture)
 
 	# Create terrain node
 	var terrain = HTerrain.new()
+	terrain.ambient_wind = wind
 	terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
 	terrain.set_data(terrain_data)
 	terrain.set_texture_set(texture_set)
+	for c in terrain_childs:
+		terrain.add_child(c)
 	add_child(terrain)
 
-func get_island_factor(x : float, z : float, center : Vector2) -> float :
+
+func generate_detail_layer_texture(terrain_data : HTerrainData, texture : Texture, density : float, shader : Shader, index : int = 0) -> HTerrainDetailLayer :
+	var detail = HTerrainDetailLayer.new()
+	detail.texture = texture
+	detail.custom_shader = shader
+	detail.view_distance = 500
+	detail.density = density
+	
+	terrain_data._edit_add_map(terrain_data.CHANNEL_DETAIL)
+	detail.layer_index = index
+	
+	return detail
+
+func generate_detail_layer_mesh(terrain_data : HTerrainData, mesh : Mesh, density : float, shader : Shader, index : int = 0) -> HTerrainDetailLayer :
+	var detail = HTerrainDetailLayer.new()
+	detail.instance_mesh = mesh
+	detail.custom_shader = shader
+	detail.view_distance = 500
+	detail.density = density
+	
+	terrain_data._edit_add_map(terrain_data.CHANNEL_DETAIL)
+	detail.layer_index = index
+	
+	return detail
+
+func generate_height(x : float, z : float, center : Vector2) -> float :
+	var h = (noise.get_noise_2d(x, z) + 1) / 2
+	return height_curve.interpolate(h) * generate_island_factor(x, z, center)
+
+func generate_island_factor(x : float, z : float, center : Vector2) -> float :
 	var distance := Vector2(x, z).distance_to(center)
 	var curve_position := distance / island_size
+	var beach_position := distance / beach_size
 	
-	return height_curve.interpolate(1.0 - curve_position)
-	# No need to call this, but you may need to if you edit the terrain later on
-	#terrain.update_collider()
+	if beach_position > 1:
+		return -island_height
+	else:
+		if curve_position > 1:
+			return curve_position - beach_position# * island_height
+		else:
+			return island_curve.interpolate(1.0 - curve_position) * island_height
+
+func get_palm() -> Mesh:
+	var mesh : CylinderMesh = CylinderMesh.new()
+	mesh.set_height(4)
+	mesh.set_bottom_radius(0.133)
+	mesh.set_top_radius(0.133)
+	return mesh
+# No need to call this, but you may need to if you edit the terrain later on
+#terrain.update_collider()
