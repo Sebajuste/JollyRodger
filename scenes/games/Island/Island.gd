d8e5421e7fa7a92113dbe6231aeb0287d7687906
Nono02P <nono02p@hotmail.fr>
2021-07-13 00:15:44 +0200

    Split the island in multiple chunk of HTerrain.

--- scenes/games/Island/Island.gd
+++ scenes/games/Island/Island.gd
@@ -1,4 +1,7 @@
-extends Node
+tool
+extends Spatial
+
+
 
 # Import classes
 const HTerrain = preload("res://addons/zylann.hterrain/hterrain.gd")
@@ -6,13 +9,14 @@ const HTerrainData = preload("res://addons/zylann.hterrain/hterrain_data.gd")
 const HTerrainTextureSet = preload("res://addons/zylann.hterrain/hterrain_texture_set.gd")
 const HTerrainDetailLayer = preload("res://addons/zylann.hterrain/hterrain_detail_layer.gd")
 
+export(float, 0.0, 1.0) var ambient_wind := 0.4
+export(int, 2, 5) var lod_scale := 2.0
 export(Shader) var detail_shader
 export(Shader) var palm_shader
 export(OpenSimplexNoise) var noise
 export(OpenSimplexNoise) var models_noise
 export(Curve) var height_curve : Curve
 export(Curve) var island_curve : Curve
-export(float) var wind = 0.4
 export(float) var grass_density = 4
 export(float) var flower_density = 1
 export(float) var palm_density = 0.04
@@ -20,11 +24,12 @@ export(float, 0, 1) var grass_on_sand = 0.6
 export(float, 0, 1) var grass_on_rock = 0.5
 export(float) var island_height = 150
 export(float) var sand_level = 5
-export(float) var terrain_size = 513
 export(float) var palms_height_threshold = 2
+export(float) var terrain_size = 257
+export(int) var chunk_width = 2
 
-var island_size : float = terrain_size / 2.3
-var beach_size : float = terrain_size / 2
+var island_size : float = terrain_size * chunk_width / 2.4
+var beach_size : float = terrain_size * chunk_width / 2
 
 var grass_texture = load("res://assets/2d/textures/terrain/TexturesCom_Grass0157_1_seamless_S.jpg")
 var sand_texture = load("res://assets/2d/textures/terrain/Sand_007_basecolor.jpg")
@@ -32,26 +37,40 @@ var rock_texture = load("res://assets/2d/textures/terrain/TexturesCom_CliffRock_
 var grass_detail = load("res://assets/2d/textures/grass.png")
 var flower_detail = load("res://assets/2d/textures/grass2.png")
 
+var _custom_shader : Shader = null
+var _custom_globalmap_shader : Shader = null
+var _texture_set := HTerrainTextureSet.new()
+
+
 func _ready():
 	# Create terrain resource and give it a size.
 	# It must be either 513, 1025, 2049 or 4097.
-	var terrain_data : HTerrainData = HTerrainData.new()
-	terrain_data.resize(terrain_size)
-	
-	var terrain_childs := Array()
-	
-	# Generate grass, flower and palms detail layer
-	terrain_childs.append(generate_detail_layer_texture(terrain_data, grass_detail, grass_density, detail_shader, 0))
-	terrain_childs.append(generate_detail_layer_texture(terrain_data, flower_detail, flower_density, detail_shader, 1))
-	terrain_childs.append(generate_detail_layer_mesh(terrain_data, get_palm(), palm_density, palm_shader, 2))
-	
-	# Commit modifications so they get uploaded to the graphics card
-	generate_map(terrain_data)
-	generate_terrain(terrain_data, terrain_childs)
-	pass
+	var space : float = (terrain_size - chunk_width - 1)
+	for i in (chunk_width * chunk_width):
+		var col = 0
+		var row = 0
+		if chunk_width > 1:
+			col = i % chunk_width
+			row = i / chunk_width
+			
+		var offset = Vector2(col * space, row * space)
+		var terrain_data : HTerrainData = HTerrainData.new()
+		terrain_data.resize(terrain_size)
+		
+		var terrain_childs := Array()
+		
+		# Generate grass, flower and palms detail layer
+		terrain_childs.append(generate_detail_layer_texture(terrain_data, grass_detail, grass_density, detail_shader, 0))
+		terrain_childs.append(generate_detail_layer_texture(terrain_data, flower_detail, flower_density, detail_shader, 1))
+		terrain_childs.append(generate_detail_layer_mesh(terrain_data, get_palm(), palm_density, palm_shader, 2))
+		
+		# Commit modifications so they get uploaded to the graphics card
+		generate_map(terrain_data, offset)
+		generate_terrain(terrain_data, terrain_childs, offset)
+		pass
 
 
-func generate_map(terrain_data : HTerrainData):
+func generate_map(terrain_data : HTerrainData, offset : Vector2 = Vector2.ZERO):
 	var heightMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
 	var normalMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
 	var splatMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
@@ -59,7 +78,7 @@ func generate_map(terrain_data : HTerrainData):
 	var flowerMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_DETAIL, 1)
 	var palmMap: Image = terrain_data.get_image(HTerrainData.CHANNEL_DETAIL, 2)
 	
-	var center = Vector2(heightMap.get_width() / 2, heightMap.get_height() / 2)
+	var center = Vector2((terrain_size  * chunk_width) / 2, (terrain_size  * chunk_width) / 2)
 	var grass_prct_sand = 1 - grass_on_sand
 	var grass_prct_rock = 1 - grass_on_rock
 	
@@ -70,14 +89,15 @@ func generate_map(terrain_data : HTerrainData):
 	flowerMap.lock()
 	palmMap.lock()
 	
-	for z in heightMap.get_height():
-		for x in heightMap.get_width():
+	for z in terrain_size + 1:
+		for x in terrain_size + 1:
 			# Generate height
-			var h = generate_height(x, z, center)
+			var v : Vector2 = Vector2(offset.x + x, offset.y + z) 
+			var h = generate_height(v.x, v.y, center)
 			
 			# Getting normal by generating extra heights directly from noise,
-			var h_right = generate_height(x + 0.1, z, center)
-			var h_forward = generate_height(x, z + 0.1, center)
+			var h_right = generate_height(v.x + 0.1, v.y, center)
+			var h_forward = generate_height(v.x, v.y + 0.1, center)
 			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
 			
 			# Generate texture amounts
@@ -91,6 +111,7 @@ func generate_map(terrain_data : HTerrainData):
 			var palm_amount : float = clamp(h - palms_height_threshold, 0, 1)
 			palm_amount = clamp(palm_amount - rock_amount, 0, 1)
 			
+			splat = splat.linear_interpolate(Color(1,0,0,0), clamp(1 - sand_amount - rock_amount, 0, 1))
 			splat = splat.linear_interpolate(Color(0,1,0,0), sand_amount)
 			splat = splat.linear_interpolate(Color(0,0,1,0), rock_amount)
 			
@@ -116,20 +137,22 @@ func generate_map(terrain_data : HTerrainData):
 	pass
 
 
-func generate_terrain(terrain_data : HTerrainData, childs : Array):
+func generate_terrain(terrain_data : HTerrainData, childs : Array, offset : Vector2 = Vector2.ZERO):
 	var terrain = HTerrain.new()
 	for c in childs:
 		terrain.add_child(c)
 	
-	terrain.ambient_wind = wind
+	terrain.ambient_wind = ambient_wind
 	terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
 	terrain.set_data(terrain_data)
-	terrain.set_texture_set(generate_texture_set())
+	generate_texture_set()
+	terrain.set_texture_set(_texture_set)
+	terrain.set_translation(Vector3(offset.x, 0, offset.y))
 	add_child(terrain)
 	pass
 
 
-func generate_texture_set() -> HTerrainTextureSet:
+func generate_texture_set():
 	var texture_set = HTerrainTextureSet.new()
 	texture_set.set_mode(HTerrainTextureSet.MODE_TEXTURES)
 	texture_set.insert_slot(-1)
@@ -138,7 +161,7 @@ func generate_texture_set() -> HTerrainTextureSet:
 	texture_set.set_texture(1, HTerrainTextureSet.TYPE_ALBEDO_BUMP, sand_texture)
 	texture_set.insert_slot(-1)
 	texture_set.set_texture(2, HTerrainTextureSet.TYPE_ALBEDO_BUMP, rock_texture)
-	return texture_set
+	set_texture_set(texture_set)
 
 
 func generate_detail_layer_texture(terrain_data : HTerrainData, texture : Texture, density : float, shader : Shader, index : int = 0) -> HTerrainDetailLayer :
@@ -174,7 +197,7 @@ func generate_island_factor(x : float, z : float, center : Vector2) -> float :
 	var beach_position := distance / beach_size
 	
 	if beach_position > 1:
-		return -island_height
+		return -island_curve.interpolate(beach_position - 1.0) * island_height
 	else:
 		if curve_position > 1:
 			return curve_position - beach_position# * island_height
@@ -191,3 +214,65 @@ func get_palm() -> Mesh:
 	
 # No need to call this, but you may need to if you edit the terrain later on
 #terrain.update_collider()
+
+func _get_property_list():
+	var props = [
+		{
+			"name": "Shader",
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_GROUP
+		},
+		{
+			"name": "custom_shader",
+			"type": TYPE_OBJECT,
+			"usage": PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "Shader"
+		},
+		{
+			"name": "custom_globalmap_shader",
+			"type": TYPE_OBJECT,
+			"usage": PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "Shader"
+		},
+		{
+			"name": "texture_set",
+			"type": TYPE_OBJECT,
+			"usage": PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE,
+			"hint": PROPERTY_HINT_RESOURCE_TYPE,
+			"hint_string": "Resource"
+		}
+	]
+	return props
+
+
+func _get(key: String):
+	if key == "texture_set":
+		return get_texture_set()
+	elif key == "custom_shader":
+		return get_custom_shader()
+	elif key == "custom_globalmap_shader":
+		return _custom_globalmap_shader
+
+func _set(key: String, value):
+	if key == "texture_set":
+		set_texture_set(value)
+	elif key == "custom_shader":
+		set_custom_shader(value)
+	elif key == "custom_globalmap_shader":
+		_custom_globalmap_shader = value
+
+
+func get_texture_set() -> HTerrainTextureSet:
+	return _texture_set
+
+func set_texture_set(new_set: HTerrainTextureSet):
+	_texture_set = new_set
+
+
+func get_custom_shader() -> Shader:
+	return _custom_shader
+
+func set_custom_shader(shader: Shader):
+	_custom_shader = shader
