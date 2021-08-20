1b635cebf30fb68e817233b434f23581c9b3758e
Sebajuste <sebajuste@hotmail.com>
2021-08-20 07:28:20 +0200

    Change shader system for the flag

--- scenes/objects/Flag/Flag.gd
+++ scenes/objects/Flag/Flag.gd
@@ -1,19 +1,28 @@
 class_name ShipFlag
 extends Spatial
 
-
+"""
 var MATERIAL_MAP := {
 	"None": "",
 	"GB": "res://scenes/objects/Flag/flag_gb.material",
 	"Spain": "res://scenes/objects/Flag/flag_spain.material",
 	"Pirate": "res://scenes/objects/Flag/flag_pirate.material"
 }
+"""
+
+const TEXTURE_MAP := {
+	"None": "",
+	"GB": "res://assets/2d/textures/flag_united_kingdom.png",
+	"Spain": "res://assets/2d/textures/flag_spain.png",
+	"Pirate": "res://assets/2d/textures/black_flag.png"
+}
 
 
 export(String, "None", "GB", "Spain", "Pirate") var faction := "None" setget set_faction
 
 
-onready var flag_mesh := $Pivot/MeshInstance
+# onready var flag_mesh := $Pivot/MeshInstance
+onready var flag_texture := $Pivot/Viewport/FlagTexture
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,23 +43,38 @@ func _enter_tree():
 #	pass
 
 
-func load_material():
+func load_texture():
+	
+	if flag_texture and faction != "None" and TEXTURE_MAP.has(faction):
+		var texture_path : String = TEXTURE_MAP[faction]
+		var texture : Texture = load(texture_path)
+		if texture:
+			#print("Load texture for %s : %s" % [owner.name, faction])
+			flag_texture.texture = texture
+		
+		pass
+	
+	"""
 	if flag_mesh and faction != "None" and MATERIAL_MAP.has(faction):
 		var material_path : String = MATERIAL_MAP[faction]
 		var material : Material = load(material_path)
 		if material:
 			print("Load material for %s : %s" % [owner.name, faction])
 			flag_mesh.set_surface_material(0, material)
+	"""
+	pass
 
 
 func set_faction(value):
 	faction = value
+	load_texture()
+	"""
 	if flag_mesh:
 		load_material()
-		"""
+		
 		if Network.enabled and is_network_master():
 			rpc("rpc_change_flag", value)
-		"""
+	"""
 
 """
 puppet func rpc_change_flag(value):
